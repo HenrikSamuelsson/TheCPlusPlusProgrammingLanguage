9b8b5faa82b580929900ebb76e425d2c6bd68fdc
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-08-31 13:04:45 +0200

    Adding Hello, world! test program.

---
+++ A-Tour-of-C-Plus-Plus-The-Basics/03/c02_e03_hello_world/main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: Henrik Samuelsson
+ *
+ * Created on den 31 augusti 2014, 12:39
+ */
+
+#include <iostream>
+
+/*
+ * Prints Hello, World! 
+ */
+int main() {
+    std::cout << "Hello, World!";
+}
+
