8d55813310deccad67cbeea485261c2a4804ec92
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-09-01 21:13:03 +0200

    Committing exercise 5 from chapter 2.

---
+++ A-Tour-of-C-Plus-Plus-The-Basics/e05_write_out/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Henrik Samuelsson
+ *
+ * Created on den 1 september 2014, 20:52
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+ * Writes out some different types of variables.
+ */
+int main() {
+
+    bool b = false;
+    char c = 'c';
+    int i = 1;
+    double d = 1.0;
+    string s = "A string.";
+    
+    cout << b << endl;
+    cout << c << endl;
+    cout << i << endl;
+    cout << d << endl;
+    cout << s << endl;
+    
+    return 0;
+}
+
