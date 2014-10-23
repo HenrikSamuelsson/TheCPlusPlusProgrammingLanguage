b165d10f1bf16b75e36d3feeb06a6438437b485b
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-10-23 22:34:28 +0200

    Added some more example of loops.

--- X_10_Statements/Loops/Loops/main.cpp
+++ X_10_Statements/Loops/Loops/main.cpp
@@ -2,7 +2,7 @@
  * File:   main.cpp
  * Author: Henrik Samuelsson
  *
- * Created on den 22 oktober 2014, 21:19
+ * Created on 22 October 2014, 21:19
  */
 
 #include <cstdlib>
@@ -11,22 +11,29 @@
 using namespace std;
 
 /*
- * 
+ * Exercise with different types of loops.
  */
 int main(int argc, char** argv) {
     const int max_length = 80;
     char input_line[max_length] = "How many types of loops exist in c++??";
     int quest_count = 0;
-    int i;
     
-    for(i=0; i!=max_length; i++)
+    for(int i=0; i!=max_length; i++)
         if(input_line[i]=='?')
             quest_count++;
     cout << "For loop found " << quest_count << " question marks.\n";
     
     quest_count = 0;
-    i = 0;
     
+    for(char* p = input_line; *p!=0; p++)
+        if(*p=='?')
+            quest_count++;
+    cout << "For loop based on pointer found " << quest_count 
+            << " question marks.\n";
+    
+    quest_count = 0;
+    
+    int i = 0;
     while(i != max_length)
     {
         if(input_line[i]=='?')
@@ -37,6 +44,13 @@ int main(int argc, char** argv) {
     }
     cout << "While loop found " << quest_count << " question marks.\n";
     
+    quest_count = 0;
+    
+    for(char &c: input_line)
+        if(c == '?')
+            quest_count++;
+    cout << "For each loop found " << quest_count << " question marks.\n";
+      
     return 0;
 }
 
